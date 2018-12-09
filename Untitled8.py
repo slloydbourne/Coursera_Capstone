ee45b5f55306c9cd3841cf03b8cc28d6cc0b7fb8
slloydbourne <45741643+slloydbourne@users.noreply.github.com>
2018-12-09 15:59:22 -0500

    Add files via upload

---
+++ Untitled8.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# This will be used for the IBM Coursera capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
